e3f70d06d3371575526cf4ab8cfd1e5b1a1c9d0a
hasbragwang <hasbragwang@tencent.com>
2025-05-09 03:36:44 +0000

    [WIP] Add the D9 training configurations: rsl_rl and skrl

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/d9/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/d9/flat_env_cfg.py
@@ -22,7 +22,7 @@ class D9FlatEnvCfg(D9RoughEnvCfg):
         self.observations.policy.height_scan = None
         # no terrain curriculum
         self.curriculum.terrain_levels = None
-        self.rewards.feet_air_time.weight = 1.0
+        self.rewards.feet_air_time.weight = 3.0
         self.rewards.feet_air_time.params["threshold"] = 0.6
 
 
