d6a48bdb6bbb3d0d9bbdc37a298b45c14437f933
hasbragwang <hasbragwang@tencent.com>
2025-05-18 10:00:50 +0000

    [Default params] Add all the default params for the scripts to modify
      * modify_weight.sh will scan flat_env_cfg.py to update the weights

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/d9/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/d9/flat_env_cfg.py
@@ -21,10 +21,27 @@ class D9FlatEnvCfg(D9RoughEnvCfg):
         self.scene.height_scanner = None
         self.observations.policy.height_scan = None
         # no terrain curriculum
-        self.rewards.lin_vel_z_l2.weight = -0.2
         self.curriculum.terrain_levels = None
-        self.rewards.feet_air_time.weight = 3.0
+        self.rewards.base_height.weight = 0.0
+        self.rewards.track_lin_vel_xy_exp.weight = 1.5
+        self.rewards.track_ang_vel_z_exp.weight = 1
+        self.rewards.lin_vel_z_l2.weight = -0.2
+        self.rewards.ang_vel_xy_l2.weight = -0.05
+        self.rewards.dof_torques_l2.weight = -1e-05
+        self.rewards.dof_acc_l2.weight = -2.5e-07
+        self.rewards.action_rate_l2.weight = -0.005
+        self.rewards.feet_air_time.weight = 1.0
         self.rewards.feet_air_time.params["threshold"] = 0.6
+        self.rewards.flat_orientation_l2.weight = -1.0
+        self.rewards.dof_pos_limits.weight = -1.0
+        self.rewards.termination_penalty.weight = -200
+        self.rewards.feet_slide.weight = -0.25
+        self.rewards.joint_deviation_hip.weight = -0.1
+        self.rewards.joint_deviation_hip_2.weight = 0.0
+        self.rewards.joint_deviation_arms.weight = -0.2
+        self.rewards.joint_deviation_arms_2.weight = 0.0
+        self.rewards.joint_deviation_torso.weight = -0.1
+        self.rewards.joint_deviation_torso_2.weight = 0.0
 
 
 class D9FlatEnvCfg_PLAY(D9FlatEnvCfg):
