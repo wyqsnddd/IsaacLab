e775bae92aa272faf5bc85aba8761700079ce188
hasbragwang <hasbragwang@tencent.com>
2025-05-23 06:59:47 +0000

    [Robot] A new robot configuration with a different joint actuation setting

--- source/isaaclab_assets/isaaclab_assets/external_assets/assets/pudu_d9.py
+++ source/isaaclab_assets/isaaclab_assets/external_assets/assets/pudu_d9.py
@@ -12,6 +12,73 @@ from isaaclab.assets import ArticulationCfg
 
 """Configuration for the Pudu D9 Humanoid robot."""
 
+PUDU_D9_12DOF_UPDATED_ACTUATOR_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=loader.get_robot_usd_path("pudu_d9", "d9_12dof.usd"),
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=8, solver_velocity_iteration_count=4
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.88),
+        joint_pos={
+            ".*_Hip_Joint_Pitch": 0.3,
+            ".*_Hip_Joint_Roll": 0.0,
+            ".*_Hip_Joint_Yaw": 0.0,
+            ".*_Knee_Joint_Pitch": -0.7,
+            ".*_Ankle_Joint_Pitch": 0.4,
+            ".*_Ankle_Joint_Roll": 0.0,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "legs": ImplicitActuatorCfg(
+            joint_names_expr=[
+                ".*_Hip_Joint_Pitch",
+                ".*_Hip_Joint_Roll",
+                ".*_Hip_Joint_Yaw",
+                ".*_Knee_Joint_Pitch",
+            ],
+            stiffness={
+                ".*_Hip_Joint_Pitch": 200.0,
+                ".*_Hip_Joint_Roll": 200.0,
+                ".*_Hip_Joint_Yaw": 200.0,
+                ".*_Knee_Joint_Pitch": 300.0,
+            },
+            damping={
+                ".*_Hip_Joint_Pitch": 2.5,
+                ".*_Hip_Joint_Roll": 2.5,
+                ".*_Hip_Joint_Yaw": 2.5,
+                ".*_Knee_Joint_Pitch": 4.0,
+            },
+            armature=0.0,
+        ),
+        "feet": ImplicitActuatorCfg(
+            joint_names_expr=[".*_Ankle_Joint_Pitch", ".*_Ankle_Joint_Roll"],
+            stiffness={
+                ".*_Ankle_Joint_Pitch": 80.0,
+                ".*_Ankle_Joint_Roll": 80.0,
+            },
+            damping={
+                ".*_Ankle_Joint_Pitch": 2.5,
+                ".*_Ankle_Joint_Roll": 2.5,
+            },
+            armature=0.00,
+        ),
+    },
+)
+
 
 PUDU_D9_12DOF_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
